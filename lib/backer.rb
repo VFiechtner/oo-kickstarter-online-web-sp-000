a6226095b0210fa89f437af4e08a64f8f112aa53
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-07-11 17:26:21 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,7 +8,7 @@ class Backer
   end
 
   def backed_project(project)
-    @backed_project << project
+    @backed_projects << project
     project.add_backer(self) unless project.backers.include?(self)
   end
 
